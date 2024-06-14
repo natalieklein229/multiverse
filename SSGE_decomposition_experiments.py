e269a1dd842db59e3b9680ccb68ff5632a769ba7
Thomas Klink Winckelman <winckelman@lanl.gov>
2024-06-14 12:01:41 -0600

    Upload New File

---
+++ SSGE_decomposition_experiments.py
@@ -0,0 +1,35 @@
+
+
+import torch
+from microsoft_365.SSGE import SpectralSteinEstimator
+from matplotlib import pyplot as plt
+import math
+import numpy as np
+from quality_of_life.my_visualization_utils import *
+from tqdm import trange
+
+torch.manual_seed(1234)
+M = 3000   # ~~~ will be implicity rounded *down* the the nearest square number: int(sqrt(M))**2
+eta = 0.0095
+make_gif = False
+D = 10
+n_test = 500
+device = "cuda" if torch.cuda.is_available() else "cpu"
+self = SpectralSteinEstimator(eta=eta)
+
+
+test_points = torch.randn( n_test, D, device=device )
+samples = torch.randn( M, D, device=device )
+xm = samples
+x = test_points
+
+
+_xm = torch.cat((x, xm), dim=-2)
+sigma = self.heuristic_sigma(_xm, _xm)
+M = torch.tensor( xm.size(-2), dtype=torch.float )
+Kxx, dKxx_dx, _ = self.grad_gram( xm, xm, sigma )
+if self.eta is not None:
+    Kxx += self.eta * torch.eye( xm.size(-2), device=xm.device )
+
+
+U,s,V = torch.linalg.svd(Kxx)
