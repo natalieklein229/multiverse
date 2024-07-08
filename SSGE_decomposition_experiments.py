c03fc7b3a276194cd9a8be48f12740ec5c824d9d
Thomas Klink Winckelman <winckelman@lanl.gov>
2024-07-08 11:25:07 -0600

    Added print statement for improved clarity

--- SSGE_decomposition_experiments.py
+++ SSGE_decomposition_experiments.py
@@ -1,30 +1,29 @@
 
-
 import torch
 from SSGE import SpectralSteinEstimator
 
-torch.manual_seed(1234)
-M = 3000   # ~~~ will be implicity rounded *down* the the nearest square number: int(sqrt(M))**2
-eta = 0.0095
-make_gif = False
-D = 10
-n_test = 500
-device = "cuda" if torch.cuda.is_available() else "cpu"
+try:
+    torch.manual_seed(1234)
+    M = 3000   # ~~~ will be implicity rounded *down* the the nearest square number: int(sqrt(M))**2
+    eta = 0.0095
+    make_gif = False
+    D = 10
+    n_test = 500
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    test_points = torch.randn( n_test, D, device=device )
+    samples = torch.randn( M, D, device=device )
+    xm = samples
+    x = test_points
+    self = SpectralSteinEstimator( eta=eta, samples=xm )
+    _xm = torch.cat((x, xm), dim=-2)
+    sigma = self.heuristic_sigma(_xm, _xm)
+    M = torch.tensor( xm.size(-2), dtype=torch.float )
+    Kxx, dKxx_dx = self.grad_gram( xm, xm, sigma )
+    if self.eta is not None:
+        Kxx += self.eta * torch.eye( xm.size(-2), device=xm.device )
+    U,s,V = torch.linalg.svd(Kxx)
+    print("Success!")
+except:
+    print("Failure...")
 
-test_points = torch.randn( n_test, D, device=device )
-samples = torch.randn( M, D, device=device )
-xm = samples
-x = test_points
-self = SpectralSteinEstimator( eta=eta, samples=xm )
-
-
-
-_xm = torch.cat((x, xm), dim=-2)
-sigma = self.heuristic_sigma(_xm, _xm)
-M = torch.tensor( xm.size(-2), dtype=torch.float )
-Kxx, dKxx_dx = self.grad_gram( xm, xm, sigma )
-if self.eta is not None:
-    Kxx += self.eta * torch.eye( xm.size(-2), device=xm.device )
-
-
-U,s,V = torch.linalg.svd(Kxx)
+#
