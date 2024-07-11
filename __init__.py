db6d98bed5ea4aab6d4b60bdab1042cfe6eacbdc
Thomas Klink Winckelman <winckelman@lanl.gov>
2024-07-10 20:22:14 -0600

    oops I forgot how __init__ works

--- __init__.py
+++
@@ -1,9 +0,0 @@
-#
-#~~~ Green outputs
-from quality_of_life.my_base_utils import colored_console_output
-colored_console_output(warn=False)
-
-#
-#~~~ Red errors
-from quality_of_life.my_base_utils import red_errors
-red_errors()
