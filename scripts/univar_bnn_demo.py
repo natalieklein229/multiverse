d79ff23fd9bbbbbfb0071d58128848d5bcac271c
Thomas Klink Winckelman <winckelman@lanl.gov>
2024-07-12 18:46:28 -0600

    made "models" a sub-directory of "bnns" (in analogy to torchvision.models)

--- scripts/univar_bnn_demo.py
+++ scripts/univar_bnn_demo.py
@@ -74,8 +74,8 @@ extra_std = False               # ~~~ if True, add the conditional std. when plo
 ## ~~~ Define the network architecture
 ### ~~~
 
-from models.univar_BNN_untrained import BNN
-from models.univar_NN_untrained  import  NN
+from bnn.models.univar_BNN_untrained import BNN
+from bnn.models.univar_NN_untrained  import  NN
 NN, BNN = NN.to(DEVICE), BNN.to(DEVICE)
 
 
