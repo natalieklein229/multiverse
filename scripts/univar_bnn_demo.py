c1e730bd902b66387b7547c8f92b1ebf30b5ad5c
Thomas Klink Winckelman <winckelman@lanl.gov>
2024-07-15 15:19:57 -0600

    Fixed imports

--- scripts/univar_bnn_demo.py
+++ scripts/univar_bnn_demo.py
@@ -78,8 +78,8 @@ extra_std = False               # ~~~ if True, add the conditional std. when plo
 ## ~~~ Define the network architecture
 ### ~~~
 
-from bnn.models.univar_BNN import BNN
-from bnn.models.univar_NN  import  NN
+from bnns.models.univar_BNN import BNN
+from bnns.models.univar_NN  import  NN
 NN, BNN = NN.to(DEVICE), BNN.to(DEVICE)
 
 
