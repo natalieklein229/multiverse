d7a0ba483c0639b4622f81f343219b7c8116bd6b
Thomas Klink Winckelman <winckelman@lanl.gov>
2024-07-16 13:59:00 -0600

    fixed dumb fatal copypaste error

--- bnns/experiments/new_trial.py
+++ bnns/experiments/new_trial.py
@@ -8,7 +8,7 @@ hyperparameters = {
     "Optimizer" : torch.optim.Adam,
     "lr" : 0.0005,
     "batch_size" : 64,
-    "n_epochs" = 200,
+    "n_epochs" : 200,
     "make_gif" : True,
     "how_often" : 10,                   # ~~~ how many snap shots in total should be taken throughout training (each snap-shot being a frame in the .gif)
     "initial_frame_repetitions" : 24,   # ~~~ for how many frames should the state of initialization be rendered
