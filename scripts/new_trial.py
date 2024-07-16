06a8de9fe1f588fae0bc0a4c8bb3b1efbdf9b65f
Thomas Klink Winckelman <winckelman@lanl.gov>
2024-07-16 13:57:15 -0600

    attempt at automatically generating new_trail.json

--- scripts/new_trial.py
+++ scripts/new_trial.py
@@ -1,4 +1,21 @@
 
-from bnns.utils import generate_json_filename
+from quality_of_life.my_base_utils import dict_to_json
 
-filename = generate_json_filename()
+hyperparameters = {
+    "DEVICE" : "cpu",
+    "seed" : 2024,
+    "dtype" : torch.float,
+    "Optimizer" : torch.optim.Adam,
+    "lr" : 0.0005,
+    "batch_size" : 64
+    "n_epochs" = 200,
+    "make_gif" : True,
+    "how_often" : 10,                   # ~~~ how many snap shots in total should be taken throughout training (each snap-shot being a frame in the .gif)
+    "initial_frame_repetitions" : 24,   # ~~~ for how many frames should the state of initialization be rendered
+    "final_frame_repetitions" : 48,     # ~~~ for how many frames should the state after training be rendered
+    "data" : "univar_missing_middle",
+    "model" : "univar_NN"
+}
+
+
+dict_to_json( hyperparameters, "new_trial.json" )
