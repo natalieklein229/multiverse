4808c774d10964cf5eb652e7a97907dad67b47ca
Thomas Klink Winckelman <winckelman@lanl.gov>
2024-07-23 18:57:29 -0600

    improved comment

--- scripts/Jacobian_memory_overflow_demo.py
+++ scripts/Jacobian_memory_overflow_demo.py
@@ -6,5 +6,5 @@ x_train = D_train.X.float()
 x = x_train[:100]
 
 #
-# ~~~ Compute the full Jacobian using torch.func
+# ~~~ On my LANL mac, this results in `zsh: killed` however, on my PC (using x=torch.randn(100,5)), I get the more informative error `torch.cuda.OutOfMemoryError: CUDA out of memory. Tried to allocate 3725.29 GiB`
 jacobians = jacrev(functional_call, argnums=1)(NN, dict(NN.named_parameters()), (x,)) # ~~~ a dictionary with the same keys as NN.named_parameters()
