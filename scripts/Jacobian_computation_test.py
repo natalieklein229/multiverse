499a57c018cb94fdd21dd46af0440feef976ef3d
Thomas Klink Winckelman <winckelman@lanl.gov>
2024-07-24 16:11:49 -0600

    manual computation of the Jacobian of final layer

--- scripts/Jacobian_computation_test.py
+++ scripts/Jacobian_computation_test.py
@@ -1,15 +1,19 @@
 
-imoprt torch
+import torch
 from torch.func import jacrev, functional_call
 from bnns.models.slosh_NN import NN
 from bnns.utils import manual_Jacobian
-
+from tqdm import trange
 
 #
 # ~~~ Make up some data
 torch.manual_seed(2024)
 x = torch.randn(50,5)
 batch_size = x.shape[0]
+number_of_output_features = NN[-1].out_features # ~~~ for slosh nets, 49719
+V = x
+for j in range(len(NN)-1):
+    V = NN[j](V)
 
-
-final_J = jacrev(functional_call, argnums=1)( NN[-1], dict(NN[-1].named_parameters()), (v,) )
+final_J = jacrev(functional_call, argnums=1)( NN[-1], dict(NN[-1].named_parameters()), (V,) )
+final_J = final_J["weight"].reshape(number_of_output_features,-1)
--- scripts/Jacobian_memory_overflow_demo.py
+++ scripts/Jacobian_memory_overflow_demo.py
@@ -1,6 +1,6 @@
 
 from torch.func import jacrev, functional_call
-from bnns.models.slosh_NN import NN
+from bnns.models.tiny_slosh_NN import NN
 from bnns.data.slosh_70_15_15 import D_train
 x_train = D_train.X.float()
 x = x_train[:100]
