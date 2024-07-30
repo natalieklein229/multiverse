4c85a9f46b59a11e1fd585d3a3b41cd7bbf1b7c3
Thomas Klink Winckelman <winckelman@lanl.gov>
2024-07-29 18:03:17 -0600

    added `fiona` to install_requires

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ setup(
     description = 'Package intended for testing (but not optimized for deploying) varions BNN algorithms',
     packages = find_packages(),    
     install_requires = [
-        "pyreadr",  # ~~~ for loading the data in .Rda format
+        "pyreadr",  # ~~~ for loading data in .Rda format
+        "fiona",    # ~~~ for loading data in .shp format
         "quality_of_life @ git+https://github.com/ThomasLastName/quality-of-life.git"
     ]
 )
