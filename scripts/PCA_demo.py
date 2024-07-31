b06f034aa32a46fdab816e9725353fde99098c2e
Thomas Klink Winckelman <winckelman@lanl.gov>
2024-07-30 19:44:26 -0600

    Created script demonstrating that many PCA components are needed

---
+++ scripts/PCA_demo.py
@@ -0,0 +1,28 @@
+
+import numpy as np
+from matplotlib import pyplot as plt
+from bnns.data.slosh_70_15_15 import out_np
+
+def make_percentage_explained_plot( M, title="Percentage of Variance Explained", show=True ):
+    m, n = M.shape
+    M = M.T if m>n else M
+    vals, vecs = np.linalg.eigh(M@M.T)
+    s = np.sqrt(vals[::-1])
+    percentage_explained = np.cumsum(s)/np.sum(s)
+    if show:
+        plt.plot(percentage_explained)
+        plt.ylim([0.9,1.])
+        plt.title(title)
+        plt.grid()
+        plt.tight_layout()
+        plt.show()
+    else:
+        return s
+
+make_percentage_explained_plot( out_np, "Percentage of Variance Explained in PCA on the Raw Data" )
+centered = out_np - np.mean(out_np,axis=0)
+make_percentage_explained_plot( centered, "Percentage of Variance Explained in PCA on the Centered Data (as in the paper)" )
+standardized = (out_np - np.mean(out_np,axis=0)) / (np.std(out_np,axis=0) +1e-10)
+make_percentage_explained_plot( centered, "Percentage of Variance Explained in PCA on the Fully Standardized Data" )
+
+
