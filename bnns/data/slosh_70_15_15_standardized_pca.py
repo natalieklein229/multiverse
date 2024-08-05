facff8e214a13b856b90f2d4a82675d0a8db6439
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-08-05 15:45:28 -0600

    Added same code as _centered_ but using standardization

--- bnns/data/slosh_70_15_15_standardized_pca.py
+++ bnns/data/slosh_70_15_15_standardized_pca.py
@@ -1,1 +1,26 @@
 
+import numpy as np
+import torch
+
+try:
+    U = np.load("slosh_standardized_U.npy")
+    s = np.load("slosh_standardized_s.npy")
+    V = np.load("slosh_standardized_V.npy")
+except:
+    from bnns.data.slosh_70_15_15 import coords_np, inputs_np, out_np, idx_train, idx_test, idx_val
+    U, s, V = np.linalg.svd( (out_np - np.mean(out_np,axis=0)) / (np.std(out_np,axis=0) +1e-10) )
+    np.save( "slosh_standardized_U.npy", U )
+    np.save( "slosh_standardized_s.npy", s )
+    np.save( "slosh_standardized_V.npy", V )
+
+#
+# ~~~ Compute indices for a train/val/test split (same code as in slosh_70_15_15.py)
+np.random.seed(2024)
+n_train = 2600
+n_test = 700
+n_val = 700
+n = len(inputs_np)
+assert len(inputs_np) == 4000 == len(out_np)
+assert n_train + n_test + n_val == n
+idx = np.random.permutation(n)
+idx_train, idx_test, idx_val = np.split( idx, [n_train,n_train+n_test] )
