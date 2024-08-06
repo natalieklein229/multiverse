f406bbaf868cb326f87f063a569ce33c922cd3b9
Thomas (Tom) Winckelman <thomas.k.winckelman@gmail.com>
2024-08-05 19:20:37 -0600

    fixed data import

--- bnns/models/slosh_centered_pca_NN.py
+++ bnns/models/slosh_centered_pca_NN.py
@@ -1,7 +1,7 @@
 
 import torch
 from torch import nn
-from slosh_70_15_15_cheap_centered_pca import r  # ~~~ the number of principal components: the output dimension of the NN
+from bnns.data.slosh_70_15_15_cheap_centered_pca import r  # ~~~ the number of principal components: the output dimension of the NN
 torch.manual_seed(2024)
 
 NN = nn.Sequential(
